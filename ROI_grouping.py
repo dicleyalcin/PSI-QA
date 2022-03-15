10643bc0e86cecee9c2371c00ff42c4b8e4899c7
Dicle Yalcin <dicle.yalcin2@gmail.com>
2022-03-14 19:55:57 -0500

    Add files via upload

--- ROI_grouping.py
+++ ROI_grouping.py
@@ -58,74 +58,92 @@ if groupPath:
 else:
     os.system("mkdir ROI_Groups")
 
-for i in class_labels:
-	cpath=Path(cwd+"/ROI_Groups"+"/"+str(i))
-	#print(cpath)
-	if os.path.isdir(cpath):
-		os.system("rm -r %s" % cpath)
-		os.system("mkdir %s" % cpath)
-	else:
-		os.system("mkdir %s" % cpath)
-#print(class_labels)
+# for each patient create a directory
+# first get patient list
 
 
-#print(onlyfiles)
+patients=[]
+for m in metadata:
+	patients.append(m[0])
 
-annot_trims = []
-for annot in range(len(onlyfiles)):
-	#print(annot)
-	at = str(Path("out_"+onlyfiles[annot].split('_Scatter')[0]+".jpg"))
-	#print(at)
+#print(patients)
+unique_patients=list(set(patients))
+
+
+for p in unique_patients:
+	ppath=Path(cwd+"/ROI_Groups"+"/"+p)
+	#print(ppath)
+	os.system("mkdir %s" % ppath)
 	#break
-	#annot_trim = at.replace(" ", "\ ")
-	#print(annot_trim)
-	annot_trims.append(at)
-	#break
-	for entry in range(len(metadata)):
-		#print(metadata[entry])
+	for i in class_labels:
+		cpath=Path(cwd+"/ROI_Groups"+"/"+p+"/"+str(i))
+		#print(cpath)
 		#break
-		dat = str(metadata[entry][0]+"-")
-		#print(dat)
-		#print(onlyfiles[annot])
+		if os.path.isdir(cpath):
+			os.system("rm -r %s" % cpath)
+			os.system("mkdir %s" % cpath)
+		else:
+			os.system("mkdir %s" % cpath)
+	#print(class_labels)
+
+
+	#print(onlyfiles)
+
+	annot_trims = []
+	for annot in range(len(onlyfiles)):
+		#print(annot)
+		at = str(Path("out_"+onlyfiles[annot].split('_Scatter')[0]+".jpg"))
+		#print(at)
 		#break
-		if dat in onlyfiles[annot]:
-			#print("here")
+		#annot_trim = at.replace(" ", "\ ")
+		#print(annot_trim)
+		annot_trims.append(at)
+		#break
+		for entry in range(len(metadata)):
+			#print(metadata[entry])
 			#break
-			dat2 = str(metadata[entry][1]+" ")
-			#print(dat2)
+			dat = str(metadata[entry][0]+"-")
+			#print(dat)
+			#print(onlyfiles[annot])
 			#break
-			if dat2 in onlyfiles[annot]:
+			if dat in onlyfiles[annot]:
 				#print("here")
 				#break
-				#dat3 =str("_"+str(metadata[entry][2]))
-				dat32 = str("_"+str(metasource[entry][6]))
-				#print(dat3)
-				#print(onlyfiles[annot])
+				dat2 = str(metadata[entry][1]+" ")
+				#print(dat2)
 				#break
-				#print(dat32)
-				#break
-				if dat32 in onlyfiles[annot]:
+				if dat2 in onlyfiles[annot]:
 					#print("here")
-					dat_class=str(metasource[entry][5])
-					#print(dat_class)
 					#break
-					#print(metadata[entry][3])
-					#print(annot_trims[annot])
-					U = Path(str(annot_trims[annot].replace(' ', "\ ")))
-					#W = Path(str(annot_trims[annot].replace(' ', "` ")))
-					W = Path(str(annot_trims[annot]))
-					if platform.system()=='Windows':
-						#command = "copy %s\ROIs_raw_results_trimmed\%s %s\%s" % (cwd, W, cwd + "\ROI_Groups", str(metasource[entry][5]))
+					#dat3 =str("_"+str(metadata[entry][2]))
+					dat32 = str("_"+str(metasource[entry][6]))
+					#print(dat3)
+					#print(onlyfiles[annot])
+					#break
+					#print(dat32)
+					#break
+					if dat32 in onlyfiles[annot]:
+						#print("here")
+						dat_class=str(metasource[entry][5])
+						#print(dat_class)
+						#break
+						#print(metadata[entry][3])
+						#print(annot_trims[annot])
+						U = Path(str(annot_trims[annot].replace(' ', "\ ")))
+						#W = Path(str(annot_trims[annot].replace(' ', "` ")))
+						W = Path(str(annot_trims[annot]))
+						if platform.system()=='Windows':
+							#command = "copy %s\ROIs_raw_results_trimmed\%s %s\%s" % (cwd, W, cwd + "\ROI_Groups", str(metasource[entry][5]))
+							#print(command)
+							wpmeta3 = str(Path(cwd + "\ROIs_raw_results_trimmed" + '\\' + str(W)))
+							#print(wpmeta3)
+							cdest = str(Path(cwd + '\ROI_Groups\\' + p + '\\' + str(metasource[entry][5])))
+							#print(cdest)
+							shutil.copy("{0}".format(wpmeta3), "{0}".format(cdest))
+						else:
+							command = "cp %s/ROIs_raw_results_trimmed/%s %s/%s/%s/" % (cwd, U, cwd+"/ROI_Groups",p,str(metasource[entry][5]))
+							os.system(command)
 						#print(command)
-						wpmeta3 = str(Path(cwd + "\ROIs_raw_results_trimmed" + '\\' + str(W)))
-						#print(wpmeta3)
-						cdest = str(Path(cwd + '\ROI_Groups\\' + str(metasource[entry][5])))
-						#print(cdest)
-						shutil.copy("{0}".format(wpmeta3), "{0}".format(cdest))
-					else:
-						command = "cp %s/ROIs_raw_results_trimmed/%s %s/%s/" % (cwd, U, cwd+"/ROI_Groups",str(metasource[entry][5]))
-						os.system(command)
-					#print(command)
-					#break
-					#os.system(command)
-#print(annot_trims)			
+						#break
+						#os.system(command)
+	#print(annot_trims)
